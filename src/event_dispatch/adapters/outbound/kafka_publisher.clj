8b778b72a400a6e603b8de0b2b0ee8980d8e40d5
Ryan Rodrigues Cordeiro <yryurodriguess@gmail.com>
2026-04-12 11:28:44 -0300

    Implement real adapter functionality with error handling and logging

--- src/event_dispatch/adapters/outbound/kafka_publisher.clj
+++ src/event_dispatch/adapters/outbound/kafka_publisher.clj
@@ -1,44 +1,92 @@
-(ns event-dispatch.adapters.outbound.kafka-publisher(ns event-dispatch.adapters.outbound.kafka-publisher)
-  (:require [event-dispatch.ports.event-publisher-port :as port]
-            [cheshire.core :as json]
+(ns event-dispatch.adapters.outbound.kafka-publisher
+  (:require [cheshire.core :as json]
             [clojure.tools.logging :as log])
-  (:import [org.apache.kafka.clients.producer KafkaProducer ProducerRecord]
-           [java.util Properties]))
+  (:import [org.apache.kafka.clients.producer KafkaProducer ProducerRecord Callback RecordMetadata]
+           [java.util Properties]
+           [java.util.concurrent Future]))
 
-(deftype KafkaEventPublisher [producer topic]
-  port/EventPublisher
-  (publish [this event]
-    (log/debug (str "Publishing event: " (:id event)))
-    (let [record (ProducerRecord. topic
-                                 (:notification-id event)
-                                 (json/generate-string event))]
-      (.send producer record)
-      (log/info (str "Event published: " (:id event)))
-      event))
+;; Kafka producer callback for handling async send results
+(defn create-callback
+  "Create a Kafka callback for handling send results"
+  [event-id]
+  (reify Callback
+    (onCompletion [this metadata exception]
+      (if exception
+        (log/error (str "Error publishing event " event-id ": " (.getMessage exception)))
+        (log/info (str "Event " event-id " published to partition "
+                      (.partition ^RecordMetadata metadata)
+                      " with offset " (.offset ^RecordMetadata metadata)))))))
 
-  (publish-batch [this events]
-    (log/debug (str "Publishing " (count events) " events"))
-    (doseq [event events]
-      (port/publish this event))
-    (log/info (str "Batch of " (count events) " events published"))))
-
-(defn create-kafka-producer
-  "Create a Kafka producer with the given configuration"
-  [bootstrap-servers]
+;; Publisher operations map for dependency injection
+(defn ->publisher
+  [bootstrap-servers topic]
+  "Create a publisher map with publish and publish-batch operations"
   (let [props (Properties.)]
     (.put props "bootstrap.servers" bootstrap-servers)
     (.put props "key.serializer" "org.apache.kafka.common.serialization.StringSerializer")
     (.put props "value.serializer" "org.apache.kafka.common.serialization.StringSerializer")
-    (KafkaProducer. props)))
+    (.put props "acks" "all")
+    (.put props "retries" "3")
+    (.put props "max.in.flight.requests.per.connection" "1")
+    
+    (let [producer (KafkaProducer. props)]
+      {:publish
+       (fn [event]
+         (try
+           (log/debug (str "Publishing event: " (:id event)))
+           (let [record (ProducerRecord. topic
+                                        (str (:notification-id event))
+                                        (json/generate-string event))
+                 callback (create-callback (:id event))]
+             (.send producer record callback)
+             (log/info (str "Event published: " (:id event)))
+             event)
+           (catch Exception e
+             (log/error (str "Error publishing event: " (.getMessage e)))
+             (throw e))))
+       
+       :publish-batch
+       (fn [events]
+         (try
+           (log/debug (str "Publishing " (count events) " events"))
+           (doseq [event events]
+             (let [record (ProducerRecord. topic
+                                          (str (:notification-id event))
+                                          (json/generate-string event))
+                   callback (create-callback (:id event))]
+               (.send producer record callback)))
+           (log/info (str "Batch of " (count events) " events published"))
+           events)
+           (catch Exception e
+             (log/error (str "Error publishing batch: " (.getMessage e)))
+             (throw e))))
+       
+       :flush
+       (fn []
+         (try
+           (log/debug "Flushing Kafka producer")
+           (.flush producer)
+           (log/info "Kafka producer flushed")
+           (catch Exception e
+             (log/error (str "Error flushing producer: " (.getMessage e)))
+             (throw e))))
+       
+       :close
+       (fn []
+         (try
+           (log/debug "Closing Kafka producer")
+           (.close producer)
+           (log/info "Kafka producer closed")
+           (catch Exception e
+             (log/error (str "Error closing producer: " (.getMessage e)))
+             (throw e))))
+       
+       :producer
+       producer
+       :topic
+       topic})))
 
 (defn create-kafka-publisher
-  "Create a Kafka-based event publisher"
+  "Deprecated: Use ->publisher instead"
   [bootstrap-servers topic]
-  (let [producer (create-kafka-producer bootstrap-servers)]
-    (KafkaEventPublisher. producer topic)))
-
-(defn close-publisher
-  "Close the Kafka publisher"
-  [publisher]
-  (.close (:producer publisher))
-  (log/info "Kafka publisher closed"))
+  (->publisher bootstrap-servers topic))
--- src/event_dispatch/domain/controllers/notification_controller.clj
+++ src/event_dispatch/domain/controllers/notification_controller.clj
@@ -2,24 +2,27 @@
   (:require [event-dispatch.domain.logic.notification-logic :as logic]
             [clojure.tools.logging :as log]))
 
-(defn create-notification-handler
+(defn create-notification
   "Controller: handle notification creation"
-  [context request]
-  (log/info "Handling create notification request")
+  [context recipient message type]
+  (log/info (str "Creating notification for: " recipient))
   (let [repository (:repository context)
-        publisher (:publisher context)]
+        publisher (:publisher context)
+        request {:recipient recipient
+                 :message message
+                 :type type}]
     (logic/create-and-publish-notification repository publisher request)))
 
-(defn get-notification-handler
+(defn get-notification
   "Controller: handle get notification request"
   [context id]
-  (log/info (str "Handling get notification request for id: " id))
+  (log/info (str "Fetching notification: " id))
   (let [repository (:repository context)]
     (logic/retrieve-notification repository id)))
 
-(defn list-notifications-handler
+(defn list-notifications
   "Controller: handle list notifications request"
   [context limit offset]
-  (log/info (str "Handling list notifications request - limit: " limit ", offset: " offset))
+  (log/info (str "Listing notifications - limit: " limit ", offset: " offset))
   (let [repository (:repository context)]
     (logic/list-notifications repository limit offset)))
